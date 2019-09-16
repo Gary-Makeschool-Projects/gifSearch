8efea6f42773fa6f174fd8fbd60599fe96432cf4
imthaghost <imthaghost@protonmail.com>
2019-09-16 04:54:41 -0700

    [update] bro its 5am i got no time for concise commit messages

--- static/jquery/auto.js
+++ static/jquery/auto.js
@@ -1,25 +1,25 @@
 // this is where the magic happens with a single post request to the server
-function doWork() {
+function magicalShit() {
     // grab the fucking data
-    var bla = $('#search').val(); // yah that shit works
+    var bla = $('#search').val();
     //lets test and see if we grabbed the data
-    console.log(bla);
-    // now create a json object out of that data
-    var myObject = new Object();
-    // assign input data into object
-    myObject.search_term = bla;
-    // yikes bro
-    var yikes = JSON.stringify(myObject);
-    // ajax the JSON to the server
-    $.post('reciever', yikes, function() {});
-    // stop link reloading the page
+    console.log(bla); // verify that shit works by opening up the console yourself
+    // now create a json object out of that data bro
+    var idkwhattonamethisobject = new Object();
+    // assign input data into object because why tf not
+    idkwhattonamethisobject.search_term = bla;
+    // yikes bro ._.
+    var yikes = JSON.stringify(idkwhattonamethisobject);
+    // ajax the JSON to the server aka send that shit to the backend
+    $.post('index', yikes, function() {});
+    // stop link reloading the page becasue that shit was pretty annoying
     event.preventDefault();
 }
 // this function does absolutley fucking nothing but just listens for changes on the client side
 $(document).ready(function() {
     $('#search').on('change keyup paste', function() {
         // lets call the magical function
-        doWork();
-        console.log('fuck yah');
+        magicalShit();
+        console.log('i sense change...im calling the backend to beat your ass');
     });
 });
